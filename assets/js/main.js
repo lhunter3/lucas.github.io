b86aaa5009bbe7ea87af231eca9f77034f800d46
Lucas Hunter <lucasbhunter@gmail.com>
2023-03-01 17:12:01 -0400

    changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: DevFolio - v4.9.0
-* Template URL: https://bootstrapmade.com/devfolio-bootstrap-portfolio-html-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 (function() {
   "use strict";
 
@@ -233,64 +227,5 @@
 
 
 
-  window.onload = () => {
-
-
-  }
-  document.getElementById('phone').addEventListener('input', function(e) {
-    var x = e.target.value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,4})/);
-    e.target.value = !x[2] ? x[1] : '(' + x[1] + ') ' + x[2] + (x[3] ? '-' + x[3] : '');
-  });
-
-
-  document.addEventListener('submit', function(event) {
-    event.preventDefault();
-
-    document.getElementById('submitButton').disabled = true;
-    document.getElementById('spin').style.display = "inline-block";
-    
-
-    
-    const data = JSON.stringify({
-      "messages": [
-        {
-          "channel": "sms",
-          "originator": "Lucas",
-          "recipients": "15064784119x",
-          "content": new FormData(event.target).get('message'),
-          "data_coding": "text"
-        }
-      ]
-    });
-
-
-    
-    
-  
-
-
-    console.log(data);
-
-    const xhr = new XMLHttpRequest();
-    xhr.withCredentials = true;
-
-    xhr.addEventListener("readystatechange", function () {
-    if (this.readyState === this.DONE) {
-      console.log(this.responseText);
-      document.getElementById('submitButton').disabled = false;
-      document.getElementById('spin').style.display = "none";
-    }
-    });
-
-    xhr.open("POST", "https://d7sms.p.rapidapi.com/messages/v1/send");
-    xhr.setRequestHeader("content-type", "application/json");
-    xhr.setRequestHeader("Token", "undefined");
-    xhr.setRequestHeader("X-RapidAPI-Key", "");
-    xhr.setRequestHeader("X-RapidAPI-Host", "d7sms.p.rapidapi.com");
-
-    xhr.send(data)
-
-
-  });
 
 })()
